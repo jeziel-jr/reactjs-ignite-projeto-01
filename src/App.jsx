717be581b620223a641303eb82c4e7e451f52fe6
jeziel-jr <jezieljunior29@hotmail.com>
2022-06-19 12:00:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Jeziel Junior" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sequi ab provident ut nobis dolore architecto at! Exercitationem eum asperiores ex delectus similique beatae facere mollitia facilis animi, non fugiat." />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1608306448197-e83633f1261c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+
+            <div className={styles.profile}>
+                <strong>Jeziel Junior</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    );
+};
