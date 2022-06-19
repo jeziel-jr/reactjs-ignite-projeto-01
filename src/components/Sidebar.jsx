68f75d4ed923017d48417bde04ddb14c7847ee7e
jeziel-jr <jezieljunior29@hotmail.com>
2022-06-19 17:12:26 -0300

    avatar component creation and responsiveness adjustments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,8 +11,8 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <Avatar src='https://github.com/jeziel-jr.png' />
 
-                <img className={styles.avatar} src="https://github.com/jeziel-jr.png" />
 
                 <strong>Jeziel Junior</strong>
                 <span>Web Developer</span>
