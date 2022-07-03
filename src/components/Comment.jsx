056d2069ebcf54354cf7d81d2dfb3c8bad62fcbc
Jeziel Junior <jezieljunior29@hotmail.com>
2022-07-03 19:47:39 -0300

    added like function

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
+
+
     return (
         <>
             <div className={styles.comment}>
@@ -16,7 +28,7 @@ export function Comment({ content }) {
                                 <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                             </div>
 
-                            <button title='Deletar comentário'>
+                            <button onClick={handleDeleteComment} title='Deletar comentário'>
                                 <Trash size={24} />
                             </button>
                         </header>
@@ -25,9 +37,9 @@ export function Comment({ content }) {
                     </div>
 
                     <footer>
-                        <button>
+                        <button onClick={handleLikeComment}>
                             <ThumbsUp />
-                            Aplaudir <span>20</span>
+                            Aplaudir <span>{likeCount}</span>
                         </button>
                     </footer>
                 </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,8 +34,23 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Por favor, digite um comentário válido.');
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        });
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <>
@@ -57,9 +72,9 @@ export function Post({ author, content, publishedAt }) {
                 <div className={styles.content}>
                     {content.map(line => {
                         if (line.type === 'paragraph') {
-                            return <p>{line.content}</p>;
+                            return <p key={line.content}>{line.content}</p>;
                         } else if (line.type === 'link') {
-                            return <p><a href="#">{line.content}</a></p>;
+                            return <p key={line.content}><a href="#">{line.content}</a></p>;
                         }
                     })}
                 </div>
@@ -72,16 +87,26 @@ export function Post({ author, content, publishedAt }) {
                         placeholder='Deixe um comentário'
                         value={newCommentText}
                         onChange={handleNewCommentChange}
+                        onInvalid={handleNewCommentInvalid}
+                        required
                     />
 
                     <footer>
-                        <button type='submit'>Publicar</button>
+                        <button type='submit' disabled={isNewCommentEmpty}>
+                            Publicar
+                        </button>
                     </footer>
                 </form>
 
                 <div className={styles.commentList}>
                     {comments.map(comment => {
-                        return <Comment content={comment} />;
+                        return (
+                            <Comment
+                                key={comment}
+                                content={comment}
+                                onDeleteComment={deleteComment}
+                            />
+                        );
                     })}
                 </div>
             </article>
